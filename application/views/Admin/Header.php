fec673b36f3e1eb5a65375064d64b53521cdd733
Kishor Pant <kishorpant1991@live.com>
2017-05-31 17:58:52 +0530

    Administrator Common added and some extra file deleted from server

---
+++ application/views/Admin/Header.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        #main-menu{display: inline-flex;padding:8px;color:black;}
+    </style>
+</head>
+<body>
+
+<div  class="container-fluid">
+    <div class="row">
+        <div class="row-menu">
+            <ul id="main-menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+            </ul>
+        </div>
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
